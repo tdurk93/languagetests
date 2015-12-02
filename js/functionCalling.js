beeb5f701a2076cdc60edc8198cb9bdde2610471
Tyler Durkota <tyler.durkota@gtri.gatech.edu>
2015-12-02 16:09:48 -0500

    Update functionCalling to test args b/w functions
    It looks like not passing a parameter to a function is the same as passing the
    value undefined.

--- js/functionCalling.js
+++ js/functionCalling.js
@@ -4,8 +4,21 @@ function myFunc (arg1, arg2, arg3) {
     // this is an idiom used to convert an arguments object to an array
     argsArray = Array.prototype.slice.call(arguments);
     console.log(argsArray.toString());
+    for (var i = 0; i < argsArray.length; i++) {
+        console.log("argument " + (i) + ": " + argsArray[i] + "(type " + typeof argsArray[i] + ")");
+    }
 }
 
+function myFunc2 (arg1, arg2, arg3, arg4) {
+    myFunc(arg1, arg2, arg3, arg4);
+}
+
+console.log("calling myFunc...");
 myFunc("hello", "foo", "bar");
 myFunc("hello");
 myFunc("hello", "foo", "bar", "baz");
+
+console.log("calling myFunc2...");
+myFunc2("hello", "foo", "bar");
+myFunc2("hello");
+myFunc2("hello", "foo", "bar", "baz");
